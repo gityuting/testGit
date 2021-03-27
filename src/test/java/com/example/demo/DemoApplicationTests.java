dc55a79b1d1140cde5caedddc463ad9db5c13fae
yuting <18301521475@163.com>
2021-03-27 16:44:49 +0800

    修改

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -29,6 +30,39 @@ class DemoApplicationTests {
 	@Test
 	void userTest(){
 		userService2.getUser();
+	}
+	@Test
+	void structure(){
+	/*	System.out.printf("%.1f",2.0-1.1);
+		System.out.printf("%.2f",2.0-1.1);
+		System.out.println(1.0-0.1);*/
+	    //数字必须加：" "
+		System.out.println(new BigDecimal(2.2));
+		System.out.println(new BigDecimal("2.0").subtract(new BigDecimal("1.1")));
+		BigDecimal bigDecimal = new BigDecimal(1.1);
+		short s=1;
+		s=(short)(s+1);
+		float f=1;
+		f=(float) (f+1d);
+		int i=1;
+		i=(int)(i+1.1);
+		long l=1;
+		l=(long)(l+1.1);
+		double d = 1;
+		d=d+1.1f;
+		s+=1;
+	}
+
+	@Test
+	public void tryfinall(){
+		System.out.println(tf());
+	}
+	public int tf(){
+		try{
+			return 1;
+		}finally {
+			return 2;
+		}
 	}
 
 }
